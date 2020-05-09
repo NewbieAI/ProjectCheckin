08f32603b23a7930ecde9ab453bab7a58ac5726f
NewbieAI <admin@tlsiejkjwiiiils.dx.am>
2020-05-09 16:01:20 -0400

    temporarily removed dependencies

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,10 +1,10 @@
 // main javascript file for index.html
 // file created on May 9th
 
-import {} from "./accoutnControl.js";
-import {} from "./tasks.js";
-import {} from "./groups.js";
-import {} from "./search.js";
+//import {} from "./accoutnControl.js";
+//import {} from "./tasks.js";
+//import {} from "./groups.js";
+//import {} from "./search.js";
 
 function main() {
     alert("main.js is loaded");
